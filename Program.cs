b64d6243dfad96c1e968976a2600c303844c27d9
Otunba-B <bryanejiofor@ymail.com>
2021-06-07 12:12:44 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace SwordDamageCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SwordDamage swordDamage = new SwordDamage();
+            Random random = new Random();
+
+            Console.WriteLine("0 for no magic/ flaming, 1 for magic, 2 for flaming, 3 for both, anything else to quit: ");
+            var line = Console.ReadKey(false).KeyChar;
+
+            if (line != '1' && line != '2' && line != '3' && line != '0')
+            {
+                return;
+            }
+
+            else
+            {
+                var Roll1 = random.Next(1, 7);
+                var Roll2 = random.Next(1, 7);
+                var Roll3 = random.Next(1, 7);
+
+                swordDamage.Roll = Roll1 + Roll2 + Roll3;
+            }
+
+            if (line == '1' || line == '3')
+            {
+                swordDamage.SetMagic(true);
+                
+            }
+            else
+            {
+                swordDamage.SetMagic(false);
+            }
+
+            if (line == '2' || line == '3')
+            {
+                swordDamage.SetFlaming(true);
+            }
+            else
+            {
+                swordDamage.SetFlaming(false);
+            }
+
+            Console.WriteLine(" Rolled " + swordDamage.Roll + " for " + swordDamage.Damage + "HP");
+        }
+    }
+}
---
+++ SwordDamage.cs
@@ -0,0 +1,40 @@
+﻿namespace SwordDamageCalculator
+{
+    class SwordDamage
+    {
+        public const int Base_Damage = 3;
+        public const int Flame_Damage = 3;
+
+        public int Roll;
+        public decimal MagicMultiplier = 1M;
+        public int FlamingDamage = 0;
+        public int Damage;
+
+        public void CalculateDamage()
+        {
+            Damage = (int)(Roll * MagicMultiplier) + Base_Damage + FlamingDamage;
+        }
+
+        public void SetFlaming(bool isFlaming)
+        {
+            CalculateDamage();
+            if(isFlaming)
+            {
+                Damage += Flame_Damage;
+            }
+        }
+
+        public void SetMagic(bool isMagic)
+        {
+            if (isMagic)
+            {
+                MagicMultiplier = 1.75M;
+            }
+            else
+            {
+                MagicMultiplier = 1M;
+            }
+            CalculateDamage();
+        }
+    }
+}
